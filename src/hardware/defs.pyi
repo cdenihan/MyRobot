c3429a65402237cbe881694dace8353ad95ac59a
Connor Denihan <188690869+cdenihan@users.noreply.github.com>
2026-04-23 17:25:41 -0400

    feat(robot): enhance hardware configuration with additional sensors and servos, update motor calibration values, and add setup mission sequence

--- src/hardware/defs.pyi
+++ src/hardware/defs.pyi
@@ -3,16 +3,39 @@
 from raccoon import AnalogSensor
 from raccoon import DigitalSensor
 from raccoon import IMU as Imu
+from raccoon import IRSensor
 from raccoon import Motor
 from raccoon.step.servo.preset import ServoPreset, _PresetPosition
 from typing import List
 
 
+class _ArmServoPreset(ServoPreset):
+    up: _PresetPosition
+    down: _PresetPosition
+
+    @property
+    def device(self) -> "Servo": ...
+
+
+class _ClawServoPreset(ServoPreset):
+    open: _PresetPosition
+    closed: _PresetPosition
+
+    @property
+    def device(self) -> "Servo": ...
+
+
 class Defs:
     imu: Imu
     button: DigitalSensor
+    front_left_ir_sensor: IRSensor
+    front_right_ir_sensor: IRSensor
+    rear_left_ir_sensor: IRSensor
+    rear_right_ir_sensor: IRSensor
     front_left_motor: Motor
     front_right_motor: Motor
     rear_left_motor: Motor
     rear_right_motor: Motor
+    arm_servo: _ArmServoPreset
+    claw_servo: _ClawServoPreset
     analog_sensors: List[AnalogSensor]
--- src/missions/m000_setup_mission.py
+++ src/missions/m000_setup_mission.py
@@ -7,6 +7,10 @@ class M000SetupMission(SetupMission):
     setup_time = 120
 
     def sequence(self) -> Sequential:
-        return seq([
-            calibrate(distance_cm=50),
-        ])
+        return seq(
+            [
+                Defs.arm_servo.up(),
+                Defs.claw_servo.open(),
+                calibrate(distance_cm=50),
+            ]
+        )
